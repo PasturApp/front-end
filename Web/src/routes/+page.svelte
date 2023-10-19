09691093a3873835612209e0952ef1ff635bec40
solp22 <sol.puentereal@gmail.com>
2023-10-19 14:02:59 -0700

    first draft dashboard, no calendar

--- Web/src/routes/+page.svelte
+++ Web/src/routes/+page.svelte
@@ -6,14 +6,35 @@
 	<div class="izquierda">
 		<div class="datos">
 			<div class="establecimiento">
-				<h1>Datos del establecimiento</h1>
+				<div>
+					<h1>Datos del establecimiento</h1>
+				</div>
+				<div class="datos-lista">
+					<ul>
+						<li>Última actualización: Septiembre</li>
+						<li>PV: 450</li>
+						<li>Producción individual: 25 lt/VO</li>
+						<li>Carga: 1.25 VO/sep</li>
+					</ul>
+				</div>
 			</div>
 			<div class="plataforma">
-				<h1>Plataforma de pastoreo</h1>
+				<div>
+					<h1>Plataforma de pastoreo</h1>
+				</div>
+				<div class="datos-lista">
+					<ul>
+						<li>Fecha: 25/09/2023</li>
+						<li>TC: 40 kgMS/ha</li>
+						<li>Stock: 2658 kgMS/ha</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 		<div class="lista">
-			<h1>Lista de tareas</h1>
+			<div>
+				<h1>Lista de tareas</h1>
+			</div>
 		</div>
 	</div>
 	<div class="derecha">
@@ -21,7 +42,12 @@
 			<h1>Calendario</h1>
 		</div>
 		<div class="evento">
-			<h1>Evento</h1>
+			<div>
+				<h1>Próximo evento</h1>
+			</div>
+			<div class="fecha">
+				Fecha 8/11/23  15 hs
+			</div>
 		</div>
 	</div>
 </div>
@@ -33,8 +59,9 @@
 	}
 
 	.izquierda {
-		@apply grid gap-3;
+		@apply grid gap-10;
 	}
+
 	.datos {
 		@apply grid gap-3;
 		grid-row: 1/2;
@@ -42,17 +69,39 @@
 
 	.establecimiento {
 		grid-column: 1/2;
-		background-color: green;
+	}
+
+	h1 {
+		@apply ml-2;
+		font-family: var(--letra_titulo);
+		color: var(--verde_oscuro);
+	}
+
+	.datos-lista, .lista div {
+		@apply p-6 pl-8 rounded-xl;
+		background-color: var(--verde_secundario);
+	}
+
+	ul {
+		@apply list-disc;
+	}
+
+	li {
+		font-family: var(--letra_titulo);
+		color: var(--blanco_fondo);
 	}
 
 	.plataforma {
 		grid-column: 2/3;
-		background-color: cadetblue;
 	}
 
 	.lista {
-		background-color: aqua;
 		grid-row: 2/3;
+	}
+
+	.lista h1 {
+		@apply text-4xl;
+		color: var(--blanco_fondo);
 	}
 
 	.derecha {
@@ -66,6 +115,12 @@
 
 	.evento {
 		grid-row: 2/3;
-		background-color: brown;
+	}
+
+	.fecha {
+		@apply p-6 pl-8 rounded-xl text-2xl;
+		background-color: var(--verde_secundario);
+		font-family: var(--letra_titulo);
+		color: var(--blanco_fondo);
 	}
 </style>
