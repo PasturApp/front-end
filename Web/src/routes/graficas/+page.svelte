99f79f8563c445f9843833f3213d4d89cd26569d
solp22 <sol.puentereal@gmail.com>
2023-10-23 08:37:47 -0700

    fix graph bug

--- Web/src/routes/graficas/+page.svelte
+++ Web/src/routes/graficas/+page.svelte
@@ -23,11 +23,6 @@
 			navigation: {
 				nextEl: '.swiper-button-next',
 				prevEl: '.swiper-button-prev'
-			},
-
-			// And if we need scrollbar
-			scrollbar: {
-				el: '.swiper-scrollbar'
 			}
 		});
 	});
