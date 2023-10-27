3fd6a1538e079d044c103a522e3b5efd9d857f7e
micaelapicco <micaelaabril421@gmail.com>
2023-10-27 16:13:31 -0300

    first version todo list

--- Web/src/routes/+page.svelte
+++ Web/src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import 'color-calendar/dist/css/theme-basic.css';
 	import 'color-calendar/dist/css/theme-glass.css';
 	import { onMount } from 'svelte';
+	import Icons from './icons.svelte';
 
 	export let data;
 	console.log(data);
@@ -20,7 +21,6 @@
 			weekdaysColor: '#0C523B'
 		});
 	});
-
 	const myEvents = [
 		{
 			start: '2021-04-15T06:00:00',
@@ -29,20 +29,30 @@
 			url: 'https://www.cssscript.com',
 			desc: 'Description 1'
 			// more key value pairs here
-		},
-		{
-			start: '2021-04-16T06:00:00',
-			end: '2021-04-16T20:30:00',
-			name: 'Event 2',
-			url: 'https://www.cssscript.com'
-		},
-		{
-			start: '2021-04-16T06:00:00',
-			end: '2021-04-17T20:30:00',
-			name: 'Event 3',
-			url: 'https://www.cssscript.com'
 		}
 	];
+	let newItem = '';
+	let todoList = [];
+
+	function removeFromList(index) {
+		todoList.splice(index, 1);
+		todoList = todoList;
+	}
+
+	function addTodo() {
+		if (newItem !== '') {
+			todoList = [
+				...todoList,
+				{
+					task: newItem,
+					completed: false
+				}
+			];
+			newItem = '';
+		}
+
+		console.log(todoList);
+	}
 </script>
 
 <div class="wrapper">
@@ -76,9 +86,35 @@
 				</div>
 			</div>
 		</div>
-		<div class="lista">
+		<div class="container">
 			<div>
 				<h1>Lista de tareas</h1>
+				<form on:submit|preventDefault={addTodo}>
+					<input
+						bind:value={newItem}
+						type="task"
+						class="todos__input"
+						placeholder="Agregar tarea"
+					/>
+					<button class="todos__button">+</button>
+				</form>
+				{#each todoList as item, index}
+					<div class="todo">
+						<span class={`todo__text ${item.completed ? 'todo__checked--strike' : ''}`}
+							>{item.task}</span
+						>
+
+						<div class="icons">
+							<button class="icon__button" on:click={() => (item.completed = !item.completed)}>
+								<Icons name="check-mark" class="icon" />
+							</button>
+
+							<button class="icon__button" on:click={() => removeFromList(index)}>
+								<Icons name="delete" class="icon" />
+							</button>
+						</div>
+					</div>
+				{/each}
 			</div>
 		</div>
 	</div>
@@ -93,7 +129,7 @@
 	</div>
 </div>
 
-<style lang="css">
+<style lang="postcss">
 	.wrapper {
 		@apply grid gap-3;
 		grid-template-columns: 2fr 1fr;
@@ -124,12 +160,13 @@
 		background-color: var(--verde_secundario);
 	}
 
-	.lista div {
+	.container div {
 		@apply p-4;
+		grid-row: 2/3;
 	}
 
 	.datos-lista {
-		@apply h-40;
+		@apply h-36;
 		line-height: 150px;
 	}
 
@@ -144,15 +181,6 @@
 
 	.plataforma {
 		grid-column: 2/3;
-	}
-
-	.lista {
-		grid-row: 2/3;
-	}
-
-	.lista h1 {
-		@apply text-4xl;
-		color: var(--blanco_fondo);
 	}
 
 	.derecha {
@@ -172,5 +200,92 @@
 		background-color: var(--verde_secundario);
 		font-family: var(--letra_titulo);
 		color: var(--blanco_fondo);
+	}
+	.container h1 {
+		@apply text-4xl;
+		color: var(--verde_oscuro);
+	}
+
+	.container {
+		@apply h-96;
+		overflow-y: scroll;
+	}
+
+	.container::-webkit-scrollbar {
+		width: 10px;
+		height: 115px;
+	}
+	.container::-webkit-scrollbar-button {
+		width: 0px;
+		height: 0px;
+	}
+
+	.container::-webkit-scrollbar-thumb {
+		background: var(--verde_oscuro);
+		border: 0px none #ffffff;
+		border-radius: 50px;
+	}
+	.container::-webkit-scrollbar-thumb:hover {
+		background: var(--verde_primario);
+	}
+	.container::-webkit-scrollbar-thumb:active {
+		background: var(--verde_secundario);
+	}
+	.container::-webkit-scrollbar-track {
+		background: #666666;
+		border: 0px none #ffffff;
+		border-radius: 50px;
+	}
+	.container::-webkit-scrollbar-track:hover {
+		background: #666666;
+	}
+	.container::-webkit-scrollbar-track:active {
+		background: #333333;
+	}
+	.container::-webkit-scrollbar-corner {
+		background: transparent;
+	}
+	.todos__input {
+		background-color: inherit;
+		border: none;
+		box-shadow: none;
+		text-decoration: none;
+		font-size: 1.2rem;
+		border-bottom: 1px solid black;
+		margin-top: 15px;
+		outline: none;
+		width: 500px;
+	}
+	.todos__button {
+		background-color: inherit;
+		border: none;
+		box-shadow: none;
+		font-size: 1.2rem;
+		cursor: pointer;
+	}
+	.todo {
+		@apply flex p-4 h-12 rounded-2xl w-10 text-lg font-extrabold;
+
+		box-shadow: 0 0 15px rgb(12 82 59 / 20%);
+		background-color: hsla(0, 0%, 100%, 0.2);
+		color: var(--verde_oscuro);
+		-webkit-backdrop-filter: blur(25px);
+		backdrop-filter: blur(25px);
+		width: inherit;
+		margin-top: 15px;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.icon__button {
+		background-color: transparent;
+		border: none;
+		box-shadow: none;
+		font-size: 1.2rem;
+		cursor: pointer;
+		color: rgba(0, 0, 0, 0.54);
+	}
+
+	.todo__checked--strike {
+		text-decoration: line-through;
 	}
 </style>
