9eed8482c788e0127066d3864d7227c922f5f68f
Patricio-Benglian <6305@holbertonstudents.com>
2023-11-10 10:16:35 -0300

    datos_establecimiento insercion funcional. ningun caso de error considerado

--- src/routes/datos_establecimiento/+page.svelte
+++ src/routes/datos_establecimiento/+page.svelte
@@ -1,7 +1,54 @@
 <script>
-	let metodos = [];
+	import { onMount } from 'svelte';
 
-	const formatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
+	let selectedMethod = '';
+
+	async function uploadEstablishment(event) {
+		event.preventDefault(); // Prevent the default form submission
+
+		const date = document.getElementById("mes").value;
+		const nvo = document.getElementById("vacas").value;
+		const avgwgt = document.getElementById("pesoVivo").value;
+		const avgprod = document.getElementById("produccion").value;
+		const stock = document.getElementById("stock").value;
+		const measurement = selectedMethod;
+
+		let uuidCookie = document.cookie
+			.split('; ')
+			.find(row => row.startsWith('uuid='))
+			?.split('=')[1];
+		uuidCookie = uuidCookie ? uuidCookie.split(',')[0] : null;
+
+		if (uuidCookie) {
+			const response = await fetch(`http://127.0.0.1:5000/api/user/${uuidCookie}/upload_establishment`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ date, nvo, avgwgt, avgprod, measurement, stock }),
+			})
+				.then(response => {
+					if (!response.ok) {
+						throw new Error("Not OK :(");
+					}
+					console.log(response);
+					return response.json();
+				})
+				.then(data => {
+					let jsonData = data;
+					// window.location.href = "http://localhost:5173/cuenta"; // Redirect to dashboard??
+				})
+				.catch(error => console.error('Error', error));
+		} else {
+			// Handle the case when uuidCookie doesn't exist
+			console.log('uuidCookie does not exist. Handle this case.');
+			window.location.href = '/inicio_sesion';
+		}
+	}
+
+	onMount(async () => {
+		// Nothing needed to happen onMount currently
+	});
 </script>
 
 <div>
@@ -27,18 +74,18 @@
 			<h2>Método de medición</h2>
 
 			{#each ['Visual', 'Regla', 'Plato medidor'] as metodo}
-				<div class="metodo">
-					<input type="radio" value={metodo} bind:group={metodos} />
-					<label class="metodo" for="metodo">{metodo}</label>
-				</div>
-			{/each}
+			<div class="metodo">
+				<input type="radio" value={metodo} bind:group={selectedMethod} />
+				<label class="metodo" for={metodo}>{metodo}</label>
+			</div>
+		{/each}
 		</div>
 		<div class="stock">
 			<label for="stock">Stock objetivo(kg/ha)</label>
-			<input type="text" id="disponibiliad" name="disponibiliad" value="2500" />
+			<input type="text" id="stock" name="stock" value="2500" />
 		</div>
 
-		<button type="submit">Ingresar</button>
+		<button type="submit" on:click={uploadEstablishment}>Ingresar</button>
 	</form>
 </div>
 
